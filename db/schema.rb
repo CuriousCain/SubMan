dbf2496693dd522b4937ae427dd770491945016c
Joel Lord <joel.lord@hotmail.co.uk>
2014-07-22 03:19:09 +0000

    Sign in/out and sessions added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140720001501) do
+ActiveRecord::Schema.define(version: 20140722010720) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -20,8 +20,10 @@ ActiveRecord::Schema.define(version: 20140720001501) do
     t.datetime "updated_at"
     t.string   "username"
     t.string   "password_digest"
+    t.string   "remember_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
